99061a9f063fd3a3439cbac7f7534d90855e902b
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-25 16:51:41 +0200

    have written the main API call function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+/**
+ * the main API hook
+ */
+function useApi(url) {
+  const [products, setProducts] = useState([]);
+  // State for holding loading state
+  const [isLoading, setIsLoading] = useState(false);
+  // State for holding error state
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        // Reset the error state in case there is an error previously
+        setIsError(false);
+        // Turn on the loading state each time we do an API call
+        setIsLoading(true);
+        const fetchedData = await fetch(url);
+        const json = await fetchedData.json();
+        console.log(json);
+        setProducts(json);
+      } catch (error) {
+        console.log(error);
+        // Set our error state to true
+        setIsError(true);
+      } finally {
+        // Clear the loading state if we get an error
+        setIsLoading(false);
+      }
+    }
+
+    getData();
+  }, [url]);
+  return { products, isLoading, isError };
+}
 
 function App() {
+  const { products, isLoading, isError } = useApi(
+    "https://v2.api.noroff.dev/online-shop"
+  );
+
+  if (isLoading) {
+    return <div>Loading</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.map((product) => (
+        <div>
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
