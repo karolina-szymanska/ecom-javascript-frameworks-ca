cb06277c9ed188547c819cb3062911871bd2ae42
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-28 01:33:03 +0200

    moved components to separate files

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
 import React from "react";
-import { Routes, Route, NavLink, Outlet } from "react-router-dom";
-import HomePage from "./pages/HomePage/index.jsx";
-import Contact from "./pages/ContactPage/index.jsx";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import Contact from "./pages/ContactPage";
 
 // function RouteNotFound() {
 //   return <div>Page not found</div>;
 // }
-
-function Nav() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">Contact</NavLink>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-// Our header component that gets used in our <Layout> component
-function Header() {
-  return (
-    <header>
-      <div>Header with Logo and nav</div>
-      <Nav />
-    </header>
-  );
-}
-
-// Our footer component that gets used in our <Layout> component
-function Footer() {
-  return <footer>Website footer</footer>;
-}
-
-// The <Outlet> from react-router-dom displays any child routes, almost like
-// passing through "children" in a component
-function Layout() {
-  return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-}
 
 function App() {
   return (
