52f7657e3221aec47eff7e0d26e2f46ff0e9a2aa
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-27 11:22:43 +0200

    removed unnecessary imports, added  <React.StrictMode>

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+
+function Home() {
+  return <div>Home</div>;
+}
+
+function Products() {
+  return <div>Products</div>;
+}
+
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/products">Products</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/products">Products</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <div>
+      <Nav />
       <Routes>
-        <Route index element={<div>Home</div>} />
-        <Route path="products" element={<div>Products</div>} />
-        <Route path="*" element={<div>Route not found</div>} />
+        <Route index element={<Home />} />
+        <Route path="products" element={<Products />} />
+        <Route path="*" element={<RouteNotFound />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
 
