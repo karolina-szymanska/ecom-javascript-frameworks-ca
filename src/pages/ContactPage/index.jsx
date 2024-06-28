0f4408689fc3a57960fa7d22e451a77c2e45ad18
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-28 17:50:02 +0200

    added a basic form

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 function Contact() {
-  return <div>Contact Page Content</div>;
+  document.title = "Contact Us | eCom";
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input
+        {...register("fullName", {
+          required: true,
+          minLength: 3,
+        })}
+      />
+      <input
+        {...register("email", {
+          pattern: /[A-Za-z]{3}/,
+          required: true,
+        })}
+      />
+      <input
+        {...register("subject", {
+          required: true,
+          minLength: 3,
+        })}
+      />
+      <input
+        {...register("body", {
+          required: true,
+          minLength: 3,
+        })}
+      />
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default Contact;
