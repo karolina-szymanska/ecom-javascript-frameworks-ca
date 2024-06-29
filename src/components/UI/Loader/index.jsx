7c8cd3a514efb500d99b30ce1b9d5272b8cbce28
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-29 16:10:04 +0200

    added aria-label attribute

--- src/components/UI/Loader/index.jsx
+++ src/components/UI/Loader/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 // Component to display a loader
 const Loader = () => {
-  return <p>Loader</p>;
+  return <p aria-label="loading">Loader</p>;
 };
 
 export default Loader;
