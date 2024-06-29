5042d3c0782d942a3aa118fd5291f2517aa3a568
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-29 16:35:43 +0200

    corrected import

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import { BackLink } from "../../components/UI/BackLink";
+import BackLink from "../../components/UI/BackLink";
 
 const schema = yup
   .object({
