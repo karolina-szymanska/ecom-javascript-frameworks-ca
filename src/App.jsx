657973429d3870675d34d5445679fee1c3b07515
Karolina Szymanska <karolinaszymanska899@gmail.com>
2024-06-29 03:09:46 +0200

    added ProductPage route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import NotFoundPage from "./pages/NotFoundPage";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 import ContactPage from "./pages/ContactPage";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
+          <Route path="product" element={<ProductPage />} />
           <Route path="contact" element={<ContactPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
